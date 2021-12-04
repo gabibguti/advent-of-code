0f724d75eea70f23b5e3ea8388e57ac597d096ea
Gabriela Gutierrez <gabibguti1@gmail.com>
2021-12-03 22:26:01 -0300

    feat: Add question 2 part 1 solving

---
+++ src/question_2_part_1.ts
@@ -0,0 +1,55 @@
+import * as fs from 'fs'
+
+type Direction = 'forward' | 'down' | 'up'
+
+type Command = {
+  direction: Direction
+  value: number
+}
+
+type Position = {
+  horizontal: number
+  depth: number
+}
+
+const LINE_BREAK = '\n'
+
+const DEFAULT_POSITION: Position = {
+  horizontal: 0,
+  depth: 0,
+}
+
+const file = fs.readFileSync('./src/question_2_part_1_input.txt', 'utf-8')
+
+const commands: Command[] = file.split(LINE_BREAK).map((line) => {
+  const [direction, value] = line.split(' ')
+  return { direction: direction as Direction, value: Number(value) }
+})
+
+const finalPosition = commands.reduce<Position>((position, command) => {
+  switch (command.direction) {
+    case 'forward':
+      return {
+        ...position,
+        horizontal: position.horizontal + command.value,
+      }
+    case 'down':
+      return {
+        ...position,
+        depth: position.depth + command.value,
+      }
+    case 'up':
+      return {
+        ...position,
+        depth: position.depth - command.value,
+      }
+  }
+
+  return position
+}, DEFAULT_POSITION)
+
+console.log('Final submarine position: ', finalPosition)
+console.log(
+  'Final submarine position multiplied: ',
+  finalPosition.horizontal * finalPosition.depth
+)
