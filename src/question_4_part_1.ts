cb69284c250bf1c7633175c674ab484ccaf3e4ca
Gabriela Gutierrez <gabibguti1@gmail.com>
2021-12-09 22:27:45 -0300

    feat: Continue question 4 part 1
    Create functional Board and find winning board

--- src/question_4_part_1.ts
+++ src/question_4_part_1.ts
@@ -1,10 +1,62 @@
 import * as fs from 'fs'
 
+class Board {
+  rows: Array<Array<number>> = []
+  cols: Array<Array<number>> = []
+  isWinner: boolean = false
+  checkedNumbers: number[] = []
+  static size: number = 5
+
+  constructor(rows: Array<Array<number>>) {
+    this.rows = Array.from(rows)
+
+    for (let colIndex = 0; colIndex < Board.size; colIndex++) {
+      const col = rows.map((row) => row[colIndex])
+      this.cols.push(col)
+    }
+  }
+
+  private isComplete = (list: number[]) => {
+    const totalChecks = list.filter((el) =>
+      this.checkedNumbers.includes(el)
+    ).length
+    const isComplete = totalChecks === list.length
+
+    return isComplete
+  }
+
+  private checkRows = () => {
+    this.rows.forEach((row) => {
+      if (this.isComplete(row)) {
+        this.isWinner = true
+        return
+      }
+    })
+  }
+
+  private checkCols = () => {
+    this.cols.forEach((col) => {
+      if (this.isComplete(col)) {
+        this.isWinner = true
+        return
+      }
+    })
+  }
+
+  public checkBoard = (drawNumber: number) => {
+    this.checkedNumbers.push(drawNumber)
+
+    this.checkRows()
+
+    if (this.isWinner) {
+      return
+    }
+
+    this.checkCols()
+  }
+}
+
 const LINE_BREAK = '\n'
-
-type Board = Array<Array<number>>
-
-type Boards = Array<Board>
 
 const file = fs.readFileSync(
   './src/question_4_part_1_example_input.txt',
@@ -19,28 +71,59 @@ const drawNumbers = drawNumbersLine
   .split(',')
   .map((number) => Number.parseInt(number, 10))
 
-const { boards } = boardsLines.reduce<{ boards: Boards; currBoard: Board }>(
-  ({ boards, currBoard }, currLine, currLineIndex) => {
+const { listOfBoardRows } = boardsLines.reduce<{
+  listOfBoardRows: Array<Array<Array<number>>>
+  currBoardRows: Array<Array<number>>
+}>(
+  ({ listOfBoardRows, currBoardRows }, currLine, currLineIndex) => {
     const isEmptyLine = !currLine
     const isLastLine = currLineIndex + 1 === boardsLines.length
 
     if (isEmptyLine) {
       // Start new board
-      return { boards: [...boards, currBoard], currBoard: [] }
+      return {
+        listOfBoardRows: [...listOfBoardRows, currBoardRows],
+        currBoardRows: [],
+      }
     }
 
-    const newBoardLine = currLine
+    const newBoardRow = currLine
       .split(' ')
       .filter((el) => !!el)
       .map((number) => Number.parseInt(number, 10))
 
-    const updatedCurrBoard = [...currBoard, newBoardLine]
+    const updatedCurrBoardRows = [...currBoardRows, newBoardRow]
 
     if (isLastLine) {
-      return { boards: [...boards, updatedCurrBoard], currBoard: [] }
+      return {
+        listOfBoardRows: [...listOfBoardRows, updatedCurrBoardRows],
+        currBoardRows: [],
+      }
     }
 
-    return { boards, currBoard: updatedCurrBoard }
+    return { listOfBoardRows, currBoardRows: updatedCurrBoardRows }
   },
-  { boards: [], currBoard: [] }
+  { listOfBoardRows: [], currBoardRows: [] }
 )
+
+const boards = listOfBoardRows.map((boardRows) => new Board(boardRows))
+
+let hasWinner: boolean = false
+
+while (!hasWinner) {
+  const drawNumber = drawNumbers.shift()
+
+  if (drawNumber === undefined) {
+    console.log(`No winners`)
+    break
+  }
+
+  boards.forEach((board, index) => {
+    board.checkBoard(drawNumber)
+    if (board.isWinner) {
+      hasWinner = true
+      console.log(`Board ${index} is the winner!`)
+      return
+    }
+  })
+}
